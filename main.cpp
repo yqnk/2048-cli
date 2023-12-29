cb75b74c792b5e869eb5ed8ed73cd7af06b9b56a
Yqnk <yan.bertile@gmail.com>
2023-12-29 01:30:24 +0100

    main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "game.h"
+#include <iostream>
+#include <string.h>
+
+int main(void)
+{
+    char *message = strdup("Welcome to 2048!");
+    
+    Game game;
+    game.update(message);    
+
+    free(message);
+
+    return 0;
+}
