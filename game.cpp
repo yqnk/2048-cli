68e6b910e8b23e59b9e4f2d87c0cad08d6dfbe07
Yqnk <yan.bertile@gmail.com>
2023-12-30 00:05:34 +0100

    Added: scores saving

--- game.cpp
+++ game.cpp
@@ -1,8 +1,11 @@
 #include "game.h"
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <string.h>
 #include <vector>
+#include <algorithm>
 
 Game::Game() {
     grid = Grid();
@@ -76,9 +79,37 @@ void Game::update(char *&message) {
     std::cout << "\033[2J\033[1;1H";
     std::cout << message << std::endl;
 
+    std::ifstream file("highscores.txt", std::ios::app);
+    std::vector<int> scores;
+    std::string line;
+
+    while (std::getline(file, line)) {
+        scores.push_back(std::stoi(line));
+    }
+
+    scores.push_back(score);
+    std::sort(scores.begin(), scores.end());   
+
+    std::ofstream file2("highscores.txt");
+    for (auto it = scores.begin(); it != scores.end(); ++it) {
+        file2 << *it << std::endl;
+    }
+    file.close();
+    file2.close();
+
     grid.print();
     std::cout << "Moves: " << moves << std::endl;
     std::cout << "Score: " << score << std::endl;
     std::cout << "Game over after " << moves << " moves!\n"
-              << "Final score: " << score << std::endl;
+              << "Final score: " << score;
+    if (scores.back() == score) {
+        std::cout << " (New high score!)" << std::endl;
+    } else {
+        int i = 1;
+        while (scores.back() != score) {
+            scores.pop_back();
+            ++i;
+        }
+        std::cout << "(#" << i << "/" << scores.size() << ")" << std::endl;
+    }
 }
